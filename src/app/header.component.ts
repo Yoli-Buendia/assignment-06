570dd991f026aa80a7c0aab152d276fd87619f96
Yoli-Buendia <bue17003@byui.edu>
2024-03-04 23:30:14 -0600

    3. Define [routerLink] Directives for Each Route

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -17,10 +17,6 @@ export class HeaderComponent {
     this.isNavbarCollapsed = !this.isNavbarCollapsed;
   }
 
-  @Output() selectedFeatureEvent = new EventEmitter<string>();
-
-  onSelected(selectedEvent: string){
-    this.selectedFeatureEvent.emit(selectedEvent);
-  }
+ constructor() {}
 
 }
